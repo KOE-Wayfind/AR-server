f9101fe4e460614bb357a42e8522ece7d29ae352
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-06-12 16:55:21 +0000

    :zap: Server ok. Added mapping localize

--- app.py
+++ app.py
@@ -20,9 +20,7 @@ def hello_world():  # put application's code here
 def process_image():
     # read json key "image_data"
     # without the `data:image/jpeg;base64,` prefix
-    print("sampai sini")
     image_data = request.json['image_data']
-
 
     # Convert the base64-encoded image data to bytes
     image_bytes = base64.b64decode(str(image_data))
@@ -42,9 +40,14 @@ def process_image():
     img = Image.open(BytesIO(image_bytes))
     img.save(temp_save_path, 'png')
 
-    resize_image(temp_save_path)
+    saved_new_path = resize_image(temp_save_path)
 
-    return 'OK'
+    result = my_hloc.check_location(saved_new_path)
+
+    if result == None:
+        return {"result": "No match found"}, 404
+
+    return {"result": result}, 200
 
 i = 0
 
@@ -86,8 +89,10 @@ def resize_image(image_path):
     # Extract the filename from original file
     file_name = Path(image_path).stem + '.png'
     
-    # Specify the temp directory path for saving the images
-    save_dir = 'images'
+    # Specify the directory path for saving the images
+    # the inference image need to be the same path as the 'training' image
+    # so, set this variable same as variable `images` in my_hloc.py:13
+    save_dir = 'koe-datasets/new'
     
     # Create the save directory if it doesn't exist
     os.makedirs(save_dir, exist_ok=True)
@@ -95,8 +100,10 @@ def resize_image(image_path):
     # Construct the full path to save the image
     temp_save_path = os.path.join(save_dir, file_name)
     
-    res_image.save(temp_save_path, 'png')
     # save the resized image
+    res_image.save(temp_save_path, 'png')
+
+    return file_name
     
 
 if __name__ == '__main__':
--- my_hloc.py
+++ my_hloc.py
@@ -2,14 +2,15 @@
 # tqdm.tqdm = tqdm.notebook.tqdm  # notebook-friendly progress bars
 from pathlib import Path
 
-from hloc import extract_features, match_features, reconstruction, visualization, pairs_from_exhaustive
+from hloc import extract_features, match_features, reconstruction, visualization, pairs_from_exhaustive, result_extractor
 from hloc.visualization import plot_images, read_image
 # from hloc.utils import viz_3d
 import numpy as np
 import pycolmap
 from hloc.localize_sfm import QueryLocalizer, pose_from_cluster
+import random
 
-images = Path('koe-datasets/raw')
+images = Path('koe-datasets/new')
 outputs = Path('outputs/demo/')
 sfm_pairs = outputs / 'pairs-sfm.txt'
 loc_pairs = outputs / 'pairs-loc.txt'
@@ -17,45 +18,88 @@ sfm_dir = outputs / 'sfm'
 features = outputs / 'features.h5'
 matches = outputs / 'matches.h5'
 
-# feature_conf = extract_features.confs['superpoint_aachen'] # for outdoor
-feature_conf = extract_features.confs['superpoint_inloc'] # for indoor
-matcher_conf = match_features.confs['superglue']
+def main():
+    # feature_conf = extract_features.confs['superpoint_aachen'] # for outdoor
+    global feature_conf, matcher_conf
+    feature_conf = extract_features.confs['superpoint_inloc'] # for indoor
+    matcher_conf = match_features.confs['superglue-fast']
 
-landmark = "e1-l2-conference-room-b*.jpg" # Use * for wildcard
-references = [str(p.relative_to(images)) for p in (images / 'day/').glob(landmark)]
-print(references)
-print(len(references), "mapping images")
-plot_images([read_image(images / r) for r in references], dpi=25)
+    def get_images(folder_path, num_images):
+        images = list(folder_path.glob('*.jpg'))
+        selected_images = random.sample(images, min(num_images, len(images)))
+        return selected_images
 
-extract_features.main(feature_conf, images, image_list=references, feature_path=features)
-pairs_from_exhaustive.main(sfm_pairs, image_list=references)
-match_features.main(matcher_conf, sfm_pairs, features=features, matches=matches);
+    # Choose between color and grayscale
+    color_mode = "gray"
 
-model = reconstruction.main(sfm_dir, images, sfm_pairs, features, matches, image_list=references)
+    # Choose the number of images from each folder
+    num_images = 12
 
-query = 'night/e1-l2-conference-room-b_3.jpg'
+    exclude_dirs = ["conference-b"]
 
-references_registered = [model.images[i].name for i in model.reg_image_ids()]
-extract_features.main(feature_conf, images, image_list=[query], feature_path=features, overwrite=True)
-pairs_from_exhaustive.main(loc_pairs, image_list=[query], ref_list=references_registered)
-match_features.main(matcher_conf, loc_pairs, features=features, matches=matches, overwrite=True);
+    references = []
+    if color_mode == 'color':
+        folder = images / 'rgb/'
+    else:
+        folder = images / 'gray/'
 
-camera = pycolmap.infer_camera_from_image(images / query)
-ref_ids = [model.find_image_with_name(n).image_id for n in references_registered]
-conf = {
-    'estimation': {'ransac': {'max_error': 12}},
-    'refinement': {'refine_focal_length': True, 'refine_extra_params': True},
-}
-localizer = QueryLocalizer(model, conf)
-ret, log = pose_from_cluster(localizer, query, camera, ref_ids, features, matches)
+    # Get the selected images from each folder
+    for sub_folder in folder.iterdir():
+        if sub_folder.is_dir() and sub_folder.name not in exclude_dirs:
+            selected_images = get_images(sub_folder, num_images)
+            references.extend([str(p.relative_to(images)) for p in selected_images])
 
-n = len(log['db'])
-inliers = np.array(log['PnP_ret']['inliers'])
-counts = n = len(log['db'])
+    print(references)
+    print(len(references), "mapping images")
 
-# display the database images with the most inlier matches
-db_sort = np.argsort(-counts)
-for db_idx in db_sort[:2]:
-  db = model.images[log['db'][db_idx]]
-  db_name = db.name
-  print(db_name)
+    extract_features.main(feature_conf, images, image_list=references, feature_path=features)
+    pairs_from_exhaustive.main(sfm_pairs, image_list=references)
+    match_features.main(matcher_conf, sfm_pairs, features=features, matches=matches);
+
+    model = reconstruction.main(sfm_dir, images, sfm_pairs, features, matches, image_list=references)
+    # check_location('night/e1-l2-conference-room-b_3.jpg')
+
+def check_location(query = str): 
+    model = pycolmap.Reconstruction(sfm_dir)
+    feature_conf = extract_features.confs['superpoint_inloc'] # for indoor
+    matcher_conf = match_features.confs['superglue-fast']
+    references_registered = [model.images[i].name for i in model.reg_image_ids()]
+    extract_features.main(feature_conf, images, image_list=[query], feature_path=features, overwrite=True)
+    pairs_from_exhaustive.main(loc_pairs, image_list=[query], ref_list=references_registered)
+    match_features.main(matcher_conf, loc_pairs, features=features, matches=matches, overwrite=True);
+
+    camera = pycolmap.infer_camera_from_image(images / query)
+    ref_ids = [model.find_image_with_name(n).image_id for n in references_registered]
+    conf = {
+        'estimation': {'ransac': {'max_error': 12}},
+        'refinement': {'refine_focal_length': True, 'refine_extra_params': True},
+    }
+    localizer = QueryLocalizer(model, conf)
+    ret, log = pose_from_cluster(localizer, query, camera, ref_ids, features, matches)
+
+    print(f'found {ret["num_inliers"]}/{len(ret["inliers"])} inlier correspondences.')
+    percent_match = (ret["num_inliers"]/len(ret["inliers"])) * 100
+    print(f'Match percentage {percent_match}')
+    possible_locations =  result_extractor.extract_loc_from_log(images, query, log, model)
+
+    print('possible_locations')
+    print(possible_locations)
+
+    # mapping to the actual location
+    if "conference-a" in possible_locations[0]:
+        return "Conference Room A"
+    elif "conference-b" in possible_locations[0]:
+        return "Conference Room B"
+    elif "enginius-office" in possible_locations[0]:
+        return "Enginius Office"
+    elif "main-entrance" in possible_locations[0]:
+        return "Main Entrance"
+    elif "oddai" in possible_locations[0]:
+        return "ODDAI"
+    elif "oscent" in possible_locations[0]:
+        return "OSCENT"
+    else :
+        return None
+
+if __name__ == "__main__":
+   main()
